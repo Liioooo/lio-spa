d10c4e630632a6d95b6d6c077a70cce5cbb94fae
Liiioooo <preissegger.leo@a1.net>
2019-06-16 14:18:57 +0200

    added error to output-emitter

--- spa-lib/output-emitter.ts
+++ spa-lib/output-emitter.ts
@@ -6,6 +6,9 @@ export class OutputEmitter<T = any> {
     private __eventName: string;
 
     public emit(data: T) {
+        if (!this.__hostElement) {
+            throw Error('The Output() decorator is missing');
+        }
         this.__hostElement.dispatchEvent(new CustomEvent(this.__eventName, {
             detail: data
         }));
