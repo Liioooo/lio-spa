0587a7039a9b2cfd0143860298bf8ccc73c20190
Liiioooo <preissegger.leo@a1.net>
2019-06-23 13:33:02 +0200

    automated release

--- scripts/publish-all.js
+++ scripts/publish-all.js
@@ -2,14 +2,25 @@ const fs = require('fs');
 const path = require('path');
 const childProcess = require('child_process');
 const simpleGit = require('simple-git')(__dirname);
+const version = require('../package').version;
 
 simpleGit.diff([], (err, data) => {
-    console.log(data);
+    if(!data) {
+        publish();
+    } else {
+        console.log('Commit all changes before publishing!');
+    }
 });
 
-// fs.readdirSync(path.join(process.cwd(), 'packages')).forEach(pack => {
-//     childProcess.exec(`cd packages && cd ${pack} && npm run publish-package`, (error, stdout, stderr) => {
-//         console.log(stdout);
-//         console.log(stderr);
-//     });
-// });
+
+function publish() {
+    simpleGit.addTag(`v${version}`, () => {
+        fs.readdirSync(path.join(process.cwd(), 'packages')).forEach(pack => {
+            childProcess.exec(`cd packages && cd ${pack} && npm run publish-package`, (error, stdout, stderr) => {
+                console.log(stdout);
+                console.log(stderr);
+            });
+        });
+        simpleGit.pushTags('origin');
+    });
+}
