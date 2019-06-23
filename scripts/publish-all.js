d9bd01ae0254030fde4c504691d1e285351328fc
Liiioooo <preissegger.leo@a1.net>
2019-06-23 13:25:13 +0200

    no diff test

--- scripts/publish-all.js
+++ scripts/publish-all.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const childProcess = require('child_process');
+const simpleGit = require('simple-git')(__dirname);
 
-fs.readdirSync(path.join(process.cwd(), 'packages')).forEach(pack => {
-    childProcess.exec(`cd packages && cd ${pack} && npm run publish-package`, (error, stdout, stderr) => {
-        console.log(stdout);
-        console.log(stderr);
-    });
+simpleGit.diff([], (err, data) => {
+    console.log(data);
 });
+
+// fs.readdirSync(path.join(process.cwd(), 'packages')).forEach(pack => {
+//     childProcess.exec(`cd packages && cd ${pack} && npm run publish-package`, (error, stdout, stderr) => {
+//         console.log(stdout);
+//         console.log(stderr);
+//     });
+// });
